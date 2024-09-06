3f8557a7fb0118df19d7a57c8bac3c307567ca66
Viktor Truderung <viktor@truderung.com>
2024-09-06 09:11:36 +0200

    marginal code hygiene

--- examples/main.bat
+++ examples/main.bat
@@ -4,20 +4,20 @@
 
 @echo off
 
-(echo. ) & echo example 1: --name is not set
+( echo. ) & echo example 1: --name is not set
 call myApp
 
-(echo. ) & echo example 2
+( echo. ) & echo example 2
 call myApp --name Peter
 
-(echo. ) & echo example 3
+( echo. ) & echo example 3
 call myApp --name Peter --tall
 
-(echo. ) & echo example 4
+( echo. ) & echo example 4
 call myApp --name Peter --tall no
 
-(echo. ) & echo example 5
+( echo. ) & echo example 5
 call myApp --name Love --gender female --tall false
 
-(echo. ) & echo example 6
+( echo. ) & echo example 6
 call myApp --name Love --gender unknown --tall false
